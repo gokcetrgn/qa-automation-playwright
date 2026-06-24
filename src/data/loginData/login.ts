5261b66bf21f4112fb23563162b0c708fc41aad8
Gokcenaz Torgan <gokcetorgan@gmail.com>
2026-06-24 15:08:08 +0300

    Implement data driven login tests across browsers and mobile devices

--- src/data/loginData/login.ts
+++ src/data/loginData/login.ts
@@ -4,3 +4,23 @@ export const loginData = {
         password: '123456789.'
     }
 };
+export const users = [
+    {
+        scenario: 'Valid Login',
+        email: 'gokcenaztorgan@gmail.com',
+        password: '123456789.',
+        expected: 'success'
+    },
+    {
+        scenario: 'Invalid Password',
+        email: 'test@test.com',
+        password: 'wrongpassword',
+        expected: 'fail'
+    },
+    {
+        scenario: 'Invalid Email',
+        email: 'wrong@test.com',
+        password: '123456',
+        expected: 'fail'
+    }
+];
--- src/tests/ui/Login.spec.ts
+++ src/tests/ui/Login.spec.ts
@@ -1,13 +1,31 @@
 import { test, expect } from '../../fixtures/pagesFixture';
-import { loginData } from '../../data/loginData/login';
+import { loginData, users } from '../../data/loginData/login';
 
-test('User should login successfully', async ({ loginPage  }) => {
-    await loginPage.navigateToLoginPage();
+//test('User should login successfully', async ({ loginPage  }) => {
+  //  await loginPage.navigateToLoginPage();
 
-    await loginPage.login(
-        loginData.validUser.email,
-        loginData.validUser.password
-    );
+    //await loginPage.login(
+      //  loginData.validUser.email,
+        //loginData.validUser.password);
 
-    await expect(loginPage.loggedInText).toBeVisible();
+//    await expect(loginPage.loggedInText).toBeVisible();});
+
+users.forEach(user => {
+
+    test(user.scenario, async ({ loginPage }) => {
+
+        await loginPage.navigateToLoginPage();
+
+        await loginPage.login(
+            user.email,
+            user.password
+        );
+
+        if (user.expected === 'success') {
+            await expect(loginPage.loggedInText).toBeVisible();
+        } else {
+            await expect(loginPage.loginErrorMessage).toBeVisible();
+        }
+    });
+
 });
